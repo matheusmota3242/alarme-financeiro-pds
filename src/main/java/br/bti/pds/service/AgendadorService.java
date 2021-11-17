6da53f1eb674682e2c1bc866d9e33cc0ad6cf5a4
matheusmota3242 <matheusmota3242@gmail.com>
2021-11-17 20:30:04 -0300

    integrando sistema com fcm

--- src/main/java/br/bti/pds/service/AgendadorService.java
+++ src/main/java/br/bti/pds/service/AgendadorService.java
@@ -2,12 +2,14 @@ package br.bti.pds.service;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import br.bti.pds.model.ParametroAcao;
+import br.bti.pds.model.PushNotificationRequest;
 import javassist.NotFoundException;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
@@ -18,15 +20,18 @@ public class AgendadorService {
 	@Autowired
 	private ParametroAcaoService service;
 	
+	@Autowired
+	private FCMService fcmService;
+	
 	@Scheduled(fixedDelay = 7000)
 	public void agendarConsulta() {
 		List<ParametroAcao> parametrosAcao = service.recuperarTodos();
 		parametrosAcao.forEach(parametroAcao -> {
 			try {
 				Stock acao = YahooFinance.get(parametroAcao.getTiquete());
-				System.out.println(String.format("Tíquete: %s; Valor: %s", acao.getName(), acao.getQuote().getPrice()));
+				fcmService.sendMessageToToken(new PushNotificationRequest(acao.getName(), null, null, null));
 				service.remover(parametroAcao.getId());
-			} catch (IOException | NotFoundException e) {
+			} catch (IOException | NotFoundException | InterruptedException | ExecutionException e) {
 				e.printStackTrace();
 			}
 		});
